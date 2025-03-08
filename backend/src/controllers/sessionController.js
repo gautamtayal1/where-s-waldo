d401c217988bb1f21fc65cfd05072ac586acb8ee
Gautam Tayal <gautamtayal65@gmail.com>
2025-03-08 23:52:24 +0530

    start/end session fixed

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -14,9 +14,17 @@ const createSession = async(req, res) => {
 const endSession = async(req, res) => {
   try {
     const {sessionId, endTime} = req.body
+    const getSession = await prisma.gameSession.findFirst({
+      where: {id: sessionId}
+    })
+    if (!getSession) {
+      return res.status(404).send('Session not found')
+    }
+    const timeTaken = Math.floor((new Date(endTime) - new Date(getSession.startTime)) / 1000);
+
     const updatedSession = await prisma.gameSession.update({
       where: {id: sessionId},
-      data: {endTime}
+      data: {endTime, timeTaken}
     })
     res.send(updatedSession)
   } catch (error) {
